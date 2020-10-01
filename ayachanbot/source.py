08ffe4b1fc60b0398d9abf51208404edb52c724f
myl7 <myl.ustc@gmail.com>
2020-10-02 02:01:30 +0800

    Fix ascii2d post request.

--- ayachanbot/source.py
+++ ayachanbot/source.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import imghdr
 
 import requests
 from bs4 import BeautifulSoup
@@ -25,18 +26,30 @@ def search_saucenao(file):
 
 
 def search_ascii2d(file):
+    def compress_html(raw):
+        return ' '.join(raw.split())
+
     token_resp = requests.get('https://ascii2d.net/')
-    token_soup = BeautifulSoup(token_resp.content.decode(), 'html.parser')
-    token = token_soup.find('input', {'name': 'authenticity_token'})['value']
+    token_content = token_resp.content.decode()
+
+    if token_resp.status_code != 200:
+        logging.error(f"ascii2d failed:{compress_html(token_content)}")
+        return
+
+    token_soup = BeautifulSoup(token_content, 'html.parser')
+    token = token_soup.select_one('input[name="authenticity_token"]')['value']
+
+    file_type = imghdr.what(file)
+    if file_type is None:
+        logging.error("ascii2d failed:unknown filetype")
+        return
+    file.seek(0)
 
     color_resp = requests.post('https://ascii2d.net/search/file', {
         'utf8': '✓',
-        "authenticity_token": token,
-    }, files={'file': file}, headers={'Content-Type': 'multipart/form-data'})
+        "authenticity_token": token
+    }, files={'file': (f'image.{file_type}', file, f'image/{file_type}')})
     color_content = color_resp.content.decode()
-
-    def compress_html(raw):
-        return ' '.join(raw.split())
 
     if color_resp.status_code != 200:
         logging.error(f"ascii2d color failed:{compress_html(color_content)}")
@@ -57,22 +70,21 @@ def search_ascii2d(file):
 
 
 def parse_ascii2d(content, base_url):
-    items = BeautifulSoup(content).select('.item-box')
+    items = BeautifulSoup(content, 'html.parser').select('.item-box')
     items.pop(0)
+
+    def parse_link_group(source):
+        link_group = [{'name': elem.string, 'url': elem['href']} for elem in source.select('a')]
+        if source.contents[0].name == 'img':
+            link_group = link_group[1:]
+        return link_group
+
     return [
         {
             'thumbnail': base_url + item.select_one('.image-box > img')['src'],
+            'hash': item.select_one('.info-box > div.hash').string,
             'size': item.select_one('.info-box > small').string,
-            'sources': [
-                {
-                    'link': source.contents[1].href,
-                    'title': source.contents[1].string,
-                    'author': source.contents[2].string,
-                    'author_link': source.contents[1].href,
-                    'site': source.contents[3].string
-                }
-                for source in item.select_one('.detail_box > h6')
-            ]
+            'links': [parse_link_group(source) for source in item.select('.detail-box > h6')]
         }
         for item in items
     ]
