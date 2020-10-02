38fd0b36bbc2df136e05c8ef726e1af8a63f3767
myl7 <myl.ustc@gmail.com>
2020-10-03 00:37:57 +0800

    Remove sep.

--- ayachanbot/reports.py
+++ ayachanbot/reports.py
@@ -1,7 +1,5 @@
 import yaml
 
-SEP = '-' * 80
-
 
 def handle_none(f):
     def wrapper(results):
@@ -16,9 +14,9 @@ def handle_none(f):
 def report_saucenao(results):
     saucenao_limit = 1
 
-    text = f'{SEP}\nSauceNAO results:\n{SEP}\n'
+    text = 'SauceNAO results:\n'
     for result in results[:saucenao_limit]:
-        text += f"{dumps(result['header'])}\n{dumps(result['data'])}{SEP}\n"
+        text += f"{dumps(result['header'])}\n{dumps(result['data'])}"
     yield text
 
 
@@ -36,19 +34,19 @@ def report_ascii2d(ascii2d_results):
         return res_list
 
     if ascii2d_results.get('color', None):
-        text = f'{SEP}\nascii2d color results:\n{SEP}\n'
+        text = 'ascii2d color results:\n'
         results = ascii2d_results['color'][:ascii2d_color_limit]
         results = preprocess_results(results)
         for result in results:
-            text += f'{dumps(result)}{SEP}\n'
+            text += dumps(result)
         yield text
 
     if ascii2d_results.get('bovw', None):
-        text = f'{SEP}\nascii2d feature results:\n{SEP}\n'
+        text = 'ascii2d feature results:\n'
         results = ascii2d_results['bovw'][:ascii2d_bovw_limit]
         results = preprocess_results(results)
         for result in results:
-            text += f'{dumps(result)}{SEP}\n'
+            text += dumps(result)
         yield text
 
 
@@ -56,10 +54,10 @@ def report_ascii2d(ascii2d_results):
 def report_whatanime(results):
     whatanime_limit = 1
 
-    text = f'{SEP}\nwhatanime results:\n{SEP}\n'
+    text = 'whatanime results:\n'
     results = results['docs'][:whatanime_limit]
     for result in results:
-        text += f'{dumps(result)}{SEP}\n'
+        text += dumps(result)
     yield text
 
 
