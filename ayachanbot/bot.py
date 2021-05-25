98d94d82ca6147a4a9aac577adbb5588085fb22a
myl7 <myl@myl.moe>
2021-05-26 06:05:29 +0800

    Init project

---
+++ ayachanbot/bot.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+from . import handlers
+
+
+def main():
+    load_dotenv()
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    token = os.getenv('BOT_TOKEN')
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', handlers.handle_start)
+    dispatcher.add_handler(start_handler)
+
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
